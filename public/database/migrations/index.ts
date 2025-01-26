2ba7e709d889aff26a2b231aada9eb9b11015cee
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-26 15:20:34 +0900

    🔥 Public/database配置ミスのため、ディレクトリ削除

--- public/database/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import drizzleConfig from "../../../drizzle.config";
-import database from "../index";
-
-export const migrateDB = async () => {
-  await migrate(database, { migrationsFolder: drizzleConfig.out });
-};
-
-migrateDB();
